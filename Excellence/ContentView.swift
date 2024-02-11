1f634fe37e8bf4f87076faa2d3ac9a9df2040859
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-11 16:08:25 -0600

    Initial Commit

---
+++ Excellence/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Excellence
+//
+//  Created by J. DeWeese on 2/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Excellence/ExcellenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExcellenceApp.swift
+//  Excellence
+//
+//  Created by J. DeWeese on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ExcellenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
